b2ce25b82ae064d84d06c9f76ec65200d279d3d9
thedritusss <hector.moreno.gallego6@gmail.com>
2025-03-09 02:38:14 +0100

    parte 2

--- Python/OOP/1.py
+++ Python/OOP/1.py
@@ -1,6 +1,7 @@
 class Employee:
+    
     num_of_emps = 0
-    raise_amount = 1.04
+    raise_amt = 1.04
     
     def __init__(self, first, last, pay):
         self.first = first
@@ -12,23 +13,19 @@ class Employee:
         
     def fullname(self):
         return '{} {}'.format (self.first, self.last)
-    
+
+    @classmethod
+    def set_raise_amt (cls, amount):
+        cls.raise_amt = amount
+
+
 
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 emp_2 = Employee("Test", "User", 60000)
 
+Employee.set_raise_amt(1.05)
 
-print(emp_1.email)
-print(emp_2.email)
-
-
-
-print(emp_1.fullname())
-print(Employee.fullname(emp_2))
-
-
-print(emp_1.raise_amount)
-
-
-print(Employee.num_of_emps)
+print(Employee.raise_amt)
+print(emp_1.raise_amt)
+print(emp_2.raise_amt)
